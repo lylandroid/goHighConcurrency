421ba8daa1f22a29e8438fdd625331e77bfbbed7
lylAndroid <lylAndroid@163.com>
2019-09-09 20:57:24 +0800

    *): 订阅者demo修改

--- RabbitMQPubSub/mainPub.go
+++ RabbitMQPubSub/mainPub.go
@@ -1,10 +1,16 @@
 package main
 
-import "../RabbitMQ"
+import (
+	"../RabbitMQ"
+	"strconv"
+	"time"
+)
 
 func main() {
-	//发送消息
-	rabbitmq := RabbitMQ.NewRabbitMQPubSub("newProduce")
-	rabbitmq.PublishPub("订阅模式，发送消息")
-
+	for i := 0; i < 100; i++ {
+		//发送消息
+		rabbitmq := RabbitMQ.NewRabbitMQPubSub("newProduce")
+		rabbitmq.PublishPub("订阅模式，发送消息 " + strconv.Itoa(i))
+		time.Sleep(time.Second)
+	}
 }
