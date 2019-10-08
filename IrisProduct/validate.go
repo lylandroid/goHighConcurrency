8a87570de3ef0d4e999fb919b86cb6d0c0779791
yilan.luo@zhangmen.com <yilan.luo@zhangmen.com>
2019-10-08 15:14:00 +0800

    +): 分布式->拦截器完成（11-5）

--- IrisProduct/validate.go
+++ IrisProduct/validate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"./common"
+	"./encrypt"
 	"fmt"
 	"github.com/kataras/iris/core/errors"
 	"net/http"
@@ -9,7 +10,35 @@ import (
 
 //统一验证拦截器，每个接口都需要提前验证
 func Auth(respW http.ResponseWriter, req *http.Request) error {
-	return errors.New("认证失败")
+	return CheckUserInfo(req)
+}
+
+func CheckUserInfo(req *http.Request) error {
+	uidCookie, err := req.Cookie("uid")
+	if err != nil {
+		return errors.New("用户未登录！")
+	}
+	signCookie, err := req.Cookie("sign")
+	if err != nil {
+		return errors.New("用户加密串获取失败！")
+	}
+	deSignByte, err := encrypt.DePwdCode(signCookie.Value)
+	if err != nil {
+		return errors.New("加密串已被篡改！")
+	}
+	fmt.Println("结果比对：uid=", uidCookie.Value, " sign: ", string(deSignByte))
+	if CheckIdInfo(uidCookie.Value, string(deSignByte)) {
+		return nil
+	}
+	return errors.New("身份校验失败!")
+}
+
+//自定义逻辑判断
+func CheckIdInfo(checkStr string, signStr string) bool {
+	if checkStr == signStr {
+		return true
+	}
+	return false
 }
 
 //执行正常业务逻辑
